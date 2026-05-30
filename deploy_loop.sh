e70615d69d2da4afdcc5f4c06973a8d328d0cd61
Med Mahmoud <med.mahmoud@dominodatalab.com>
2026-05-30 10:54:16 +0000

    Modernize

--- deploy_loop.sh
+++ deploy_loop.sh
@@ -2,5 +2,5 @@
 while true
 do
     [[ -n "$(git status --porcelain)" ]] || { git fetch origin main && git reset --hard FETCH_HEAD; }
-    python ./bot_main.py
+    uv run python ./bot_main.py
 done
